0dab809d1d8241dfcc5afb0454dd3242406f3dfc
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2024-05-03 14:32:23 -0300

    :tada: primeiro commit

---
+++ src/main/java/com/alcd/appbackend/AppbackendApplication.java
@@ -0,0 +1,13 @@
+package com.alcd.appbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbackendApplication.class, args);
+	}
+
+}
