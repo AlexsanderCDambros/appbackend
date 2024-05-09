79ffe155c449ef361f022ccb378300f8f39c5c6b
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2024-05-08 22:38:34 -0300

    :sparkles: configuracao do spring boot security

--- src/main/java/com/alcd/appbackend/config/SecurityConfig.java
+++ src/main/java/com/alcd/appbackend/config/SecurityConfig.java
@@ -1,9 +1,40 @@
 package com.alcd.appbackend.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class SecurityConfig {
-
 	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/health/**",
+			"/h2-console/**"
+	};
+	
+	@Bean
+    SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+		
+        http
+        	.cors(cors -> cors.disable())
+        	.csrf((csrf) -> csrf.disable())
+            .authorizeHttpRequests((authz) -> authz
+                .requestMatchers(PUBLIC_MATCHERS).permitAll()
+            	.anyRequest().authenticated()
+            )
+            .headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions
+                    .sameOrigin()
+                )
+            )
+            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+
 }
